be1bcd18b3f8034d7ec2cff85e2d6946ffd58a38
AaronAE <espinozaea@outloo.com>
2023-06-05 18:16:13 -0500

    ejercicio 10 subido desde ubuntu

--- src/ejercicios/EJERCICIO10.java
+++ src/ejercicios/EJERCICIO10.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejercicios;
-
+import java.util.Scanner;
 /**
  *
  * @author espin
@@ -16,6 +16,20 @@ public class EJERCICIO10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite un numero positivo");
+        int limitePositivo = scanner.nextInt();
+
+        int suma;
+        do {
+            suma=0;
+            System.out.println("Digite el primer numero");
+            int primerNumero = scanner.nextInt();
+            System.out.println("Digite el segundo numero");
+            int segundoNumero = scanner.nextInt();
+            suma = primerNumero + segundoNumero;
+        } while(limitePositivo > suma);
     }
     
 }
